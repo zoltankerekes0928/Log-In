934fc5434457c400b7599576c436e25d02b2f124
Zoltan Kerekes <73471377+zoltankerekes0928@users.noreply.github.com>
2021-09-21 13:23:07 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+//const form = document.getElementById("email")
+const email = document.getElementById("email")
+const text = document.getElementById("text")
+const pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/gm
+const patternpsw = /^\w.{8,12}$/gm
+const psw = document.getElementById('password')
+const pswtext = document.getElementById('pswtext')
+
+function mailValidation(){
+  const mailData = document.getElementById("email").value
+  if(mailData.match(pattern)){
+    text.innerText="Valid email adress"
+    text.classList.remove('invalid')
+    text.classList.add('valid')
+  }else{
+    text.innerText="Invalid email adress"
+    text.classList.remove('valid')
+    text.classList.add('invalid')
+  }
+}
+function pswValidation(){
+  const pswData = psw.value
+  if(pswData.match(patternpsw)){
+    pswtext.innerText="Valid password"
+    pswtext.classList.remove('invalid')
+    pswtext.classList.add('valid')
+  }else{
+    pswtext.innerText="Invalid password"
+    pswtext.classList.remove('valid')
+    pswtext.classList.add('invalid')
+  }
+}
